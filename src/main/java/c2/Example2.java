cf41246d5e6b8b3b46ff94d025515cea28dee9e8
krishna <krishnamsmail@gmail.com>
2021-11-02 08:35:06 -0400

    New Test Codes

---
+++ src/main/java/c2/Example2.java
@@ -0,0 +1,11 @@
+package c2;
+
+public class Example2 {
+
+    public static void main(String[] args) {
+        int num = 100;
+        System.out.println("Num is : "+num);
+        num = num * 20;
+        System.out.println("Num now is : "+num);
+    }
+}
